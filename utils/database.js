edb668b52914b861d9cb283e3c4bd928640ef8d1
komal <khunekomal08@gmail.com>
2026-08-20 11:55:49 +0530

    mongourl

--- utils/database.js
+++ utils/database.js
@@ -3,7 +3,9 @@ const MongoClient=mongodb.MongoClient;
 let _db;
 
 const mongoconnect=(callback)=>{
-    MongoClient.connect("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.7.0")
+    // MongoClient.connect("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.7.0")
+    MongoClient.connect("mongodb+srv://komalkhune_08:komal4093@cluster0.r4iesua.mongodb.net/?appName=Cluster0")
+
     .then((success)=>{
         console.log(success)
         _db=success.db("mydatabace");
